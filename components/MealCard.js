9682c40608eb16e3f669a41428e68037d5aad39d
Emmanuel_A37 <akanjiemmanuel37@gmail.com>
2024-09-18 17:44:08 +0100

    updated meal card

--- components/MealCard.js
+++ components/MealCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { FaYoutube } from 'react-icons/fa';
+
 
 const MealCard = ({ meal }) => {
     return (
@@ -11,8 +11,6 @@ const MealCard = ({ meal }) => {
                     alt={meal.strMeal}
                     className='w-full  object-cover rounded-t-lg transition-opacity duration-300 hover:opacity-30'
                 />
-                <div className='absolute top-18 left-16 -right-7  z-10 flex items-center justify-center w-12 h-12 bg-red-600 rounded-full text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
-                    <FaYoutube className='text-2xl' />
                 </div>
                 <div className='p-4'>
                     <h3 className='text-center text-lg font-semibold h-20  overflow-hidden text-gray-800'>
